dbf0a4d6707f973b0c2d6b8c7c012a8f85ddf384
Pavlatko <pavlatko@seznam.cz>
2026-02-22 19:41:36 +0100

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,1 +1,24 @@
-﻿sw.js
+﻿const CACHE_NAME = 'flashcards-v1';
+const ASSETS = [
+  './',
+  './index.html',
+  './sw.js'
+];
+
+// Instalace a kešování souborů
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS);
+    })
+  );
+});
+
+// Obsluha požadavků (umožňuje offline provoz)
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
